49293675685f424fa5c61cf9abfaf05c622584da
DESKTOP-A5I2P67\persons <kjhwang1014@naver.com>
2021-03-11 02:14:25 +0900

    책추가,파티클클릭시재생,3클릭시재생

---
+++ Assets/step03/Scripts/snowtank.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snowtank : MonoBehaviour
+{
+    bool isMove = false;
+    private Vector3 touchpos;
+    private Animator animator;
+    public ParticleSystem snow;
+
+    // Use this for initialization 
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+    // Update is called once per frame 
+    void Update()
+    {
+        animator.SetBool("snow", false);
+        touchClick();
+    }
+    // 터치 시 오브젝트 확인 함수 
+    void touchClick()
+    {
+        // 터치 입력이 들어올 경우
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 오브젝트 정보를 담을 변수 생성 
+            RaycastHit hit;
+            // 터치 좌표를 담는 변수 
+            Ray touchray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // 터치한 곳에 ray를 보냄 
+            Physics.Raycast(touchray, out hit); // ray가 오브젝트에 부딪힐 경우 
+
+            if (hit.collider.gameObject.name == "Cylinder310" ||
+                hit.collider.gameObject.name == "Cylinder311" ||
+                hit.collider.gameObject.name == "Sphere029")
+            {
+                if (isMove == false)
+                {
+                    Invoke("touchClick", 5.0f);
+                    isMove = true;
+                    animator.SetBool("snow", true);
+                    snow.Play();
+
+                }
+                else
+                {
+                    isMove = false;
+                    animator.SetBool("snow", false);
+                }
+
+                Debug.Log(hit.collider.gameObject.name);
+
+            }
+
+        }
+
+    }
+}
